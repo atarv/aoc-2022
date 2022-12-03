9020989276765bd010e515ac545d496c54d4bca4
Aleksi Tarvainen <a.aleksi.tarvainen@gmail.com>
2022-12-03 11:45:02 +0200

    Day 1 solutions

---
+++ day01-part1.rkt
@@ -0,0 +1,9 @@
+#lang racket
+;; Day 1: Calorie Counting
+
+(let* ([input (port->string (current-input-port))]
+       [calories (map
+                  (lambda (str) (map string->number (port->lines (open-input-string str))))
+                  (string-split input #px"\n\n"))]
+       [calory-sums (map (lambda (group) (apply + group)) calories)])
+  (apply max calory-sums))
